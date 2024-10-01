770b023a1b48d5f18ce9809f1f4f65b73ce62194
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-10-01 10:05:02 +0600

    step 4: create useeffect to connect to websocket, add useeffect to reDraw on canvas if websocket send new coordinates, update ondrawend to send new drawed coordinates,update onCanvasClick to draw on mouse position after click and hold move

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,88 @@
-import {ChangeEvent, useRef, useState} from 'react';
+import {ChangeEvent, useEffect, useRef, useState} from 'react';
+import {Coordinates, IncomingMessage} from './types.ts';
 
 
 const App = () => {
+  const ws = useRef<WebSocket | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [isMouseDown, setMouseDown] = useState(false);
+  const [coordinates, setCoordinates] = useState<Coordinates[]>([]);
   const [newCoordinates, setNewCoordinates] = useState<Coordinates[]>([]);
   const [color, setColor] = useState<string>('#000000');
 
+  useEffect(() => {
+    ws.current = new WebSocket(`ws://localhost:8000/canvas`);
+    ws.current.onclose = () => console.log('WS disconnected');
+    ws.current.onmessage = (event) => {
+      const decodedMessages = JSON.parse(event.data) as IncomingMessage;
+      if (decodedMessages.type === 'NEW_COORDINATE') {
+        setCoordinates((prevState) => [...prevState, ...decodedMessages.payload]);
+      }else if (decodedMessages.type === 'OLD_COORDINATE') {
+        setCoordinates([...decodedMessages.payload]);
+      }
+    };
+    return () => {
+      ws.current?.close();
+    };
+  }, []);
 
+  useEffect(() => {
+    const draw = (ctx: CanvasRenderingContext2D) => {
+      ctx.clearRect(0, 0, 200, 200);
+      for (let i = 0; i < coordinates.length; i++) {
+        ctx.fillStyle = coordinates[i].color;
+        ctx.fillRect(coordinates[i].x, coordinates[i].y, 1, 1);
+      }
+    };
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext('2d');
+    if (context) {
+      draw(context);
+    }
+  }, [coordinates]);
 
   const onDrawStart = () => {
     if (!isMouseDown) {
       setMouseDown(true);
     }
-  }
+  };
   const onDrawEnd = () => {
     if (isMouseDown) {
       setMouseDown(false);
+      if (!ws.current) {
+        return;
+      }
+      ws.current.send((JSON.stringify({
+        type: 'SEND_COORDINATES',
+        payload: newCoordinates,
+      })));
+      setNewCoordinates([]);
     }
-  }
+  };
   const onColorChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setColor(event.target.value)
-  }
+    setColor(event.target.value);
+  };
   const onCanvasClick = (event: MouseEvent) => {
     if (isMouseDown) {
-      console.log('drawing');
+      const canvas = canvasRef.current;
+      const bounding = canvas?.getBoundingClientRect();
+      const context = canvas?.getContext('2d');
+      if (bounding && context) {
+        setNewCoordinates(prevState => [...prevState,
+          {
+            x: (event.clientX - bounding.left) / 3,
+            y: (event.clientY - bounding.top) / 6,
+            color: color,
+          }
+        ]);
+        context.fillStyle = color;
+        context.fillRect((event.clientX - bounding.left) / 3, (event.clientY - bounding.top) / 6, 1, 1);
+      }
     }
-  }
-
-
+  };
   return (
     <div>
-      <input type='color' value={color} onChange={onColorChange}/>
+      <input type="color" value={color} onChange={onColorChange}/>
       <canvas ref={canvasRef}
               onMouseOut={onDrawEnd}
               onMouseDown={onDrawStart}
@@ -39,7 +90,7 @@ const App = () => {
               onMouseMove={onCanvasClick}
               style={{height: '900px', width: '900px', border: '1px solid black'}}></canvas>
     </div>
-  )
+  );
 };
 
-export default App
+export default App;
